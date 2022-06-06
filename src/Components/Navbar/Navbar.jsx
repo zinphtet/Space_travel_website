372624c2af69c724a7e746297dbcdd3744919657
Zin PHtet <zinpainghtet.215108@gmail.com>
2022-06-06 16:29:04 +0630

    home

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import './Navbar.scss'
 import Logo from '../../assets/shared/logo.svg'
 import Humber from '../../assets/shared/icon-hamburger.svg'
@@ -6,21 +6,25 @@ import Close from '../../assets/shared/icon-close.svg'
 import { Routes, Route, Link, useLocation } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 const Navbar = () => {
-  
+  const [navOpen , setNavOpen] = useState(false)
+
+  const handleNav = ()=>{
+    setNavOpen( (state) =>!state)
+  }
   return (
     <div className="navbar">
         <div className="logo">
           <img src={Logo} alt="This is a logo" />
         </div>
        
-        <ul className='navlinks'>
-          <li className={`navlink nav_link_item`}><NavLink to='/' > <span>00</span> HOME</NavLink></li>
-          <li className='navlink nav_link_item'><NavLink to='/destination'><span>01</span>Destination</NavLink></li>
-          <li className='navlink nav_link_item'><NavLink to='/crew'><span>02</span> Crew</NavLink></li>
-          <li className='navlink nav_link_item'><NavLink to='/technology'> <span>03</span>Technology</NavLink></li>
+        <ul className={`navlinks ${navOpen? 'open' : ''}`} >
+          <li className={`navlink nav_link_item`} onClick={handleNav}><NavLink to='/' > <span>00</span> HOME</NavLink></li>
+          <li className='navlink nav_link_item' onClick={handleNav}><NavLink to='/destination'><span>01</span>Destination</NavLink></li>
+          <li className='navlink nav_link_item' onClick={handleNav}><NavLink to='/crew'><span>02</span> Crew</NavLink></li>
+          <li className='navlink nav_link_item' onClick={handleNav}><NavLink to='/technology'> <span>03</span>Technology</NavLink></li>
         </ul>
         <div className="menu">
-          <img src={Humber} alt="menu icon" />
+          <img src={navOpen ?Close : Humber} alt="menu icon" onClick={handleNav} />
         </div>
     </div>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,25 @@ import './Home.scss'
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <div className="home_space flex flex-center">
+         <div className="inner_space">
+         <p className="home_travel">
+            so,you want to travel to
+          </p>
+          <p className="home_title">space</p>
+          <p className="para">
+          Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!
+          </p>
+         </div>
+         
+      </div>
+      <div className="home_explore flex flex-center">
+           <div className="explore  flex flex-center">
+                  Explore
+           </div>
+      </div>
+    </div>
   )
 }
 
